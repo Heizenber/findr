9f2cd40d91a5d556db829bcfad455a94845888c5
Heizenberg <popov.roman91@gmail.com>
2023-10-26 20:48:13 +0300

    finished functionality of get_args function

--- src/lib.rs
+++ src/lib.rs
@@ -24,7 +24,65 @@ pub fn get_args() -> MyResult<Config> {
         .version("0.1.0")
         .author("Roman Popov <example@gmail.com>")
         .about("Rust find")
+        .arg(
+            Arg::with_name("paths")
+                .value_name("PATH")
+                .help("Search paths")
+                .default_value(".")
+                .multiple(true)
+        )
+        .arg(
+            Arg::with_name("names")
+                .short("n")
+                .long("name")
+                .value_name("NAME")
+                .help("Name")
+                .takes_value(true)
+                .multiple(true)
+        )
+        .arg(
+            Arg::with_name("types")
+                .short("t")
+                .long("type")
+                .value_name("TYPE")
+                .help("Entry type")
+                .possible_values(&["f", "d", "l"])
+                .takes_value(true)
+                .multiple(true)
+        )
         .get_matches();
+
+    let names = matches.values_of_lossy("names")
+        .map(|vals| {
+            vals.into_iter()
+                .map(|name| {
+                    Regex::new(&name)
+                        .map_err(|_| format!("Invalid --name \"{}\"", name))
+                })
+                .collect::<Result<Vec<_>, _>>()
+        })
+        .transpose()?
+        .unwrap_or_default();
+
+    let entry_types = matches.values_of_lossy("types")
+        .map(|vals| {
+            vals.iter()
+                .map(|val| match val.as_str() {
+                    "d" => Dir,
+                    "f" => File,
+                    "l" => Link,
+                    _ => unreachable!("Invalid type")
+                })
+                .collect()
+        })
+        .unwrap_or_default();
+
+    let paths = matches.values_of_lossy("paths").unwrap();
+    Ok(
+        Config { paths, names, entry_types }
+    )
+
+
 }
 
 pub fn run(config: Config) -> MyResult<()> {
